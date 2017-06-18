9391e92bb94d9f355008ca726c6e5085c0c18670
cindy <cindialy@gmail.com>
2017-06-17 22:00:19 -0400

    avances html js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 /*aquí va tu código*/
+
+function init(){
+	var guardado= document.getElementById("guardar");
+	guardado.addEventListener("click", guardarDatos);
+}
+
+function guardarDatos(){
+	var dato1 = document.getElementById("clave").value;
+	var dato2 = document.getElementById("valor").value;
+
+	localStorage.setItem(dato1, dato2);
+
+	var contenedor
+}
